4ede2b39e0a097b08317cd109eb889ccdcfd943c
NITIN M. TELANG <nitintelang110@gmail.com>
2023-12-13 16:43:00 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
+import React,{ useState } from 'react';
+import './App.css';
 
+const Counter=()=> {
 
-import './App.css'
-
-function App() {
+  const [counter,setCounter]=useState(0);
+   
+  const Increament=()=>{
   
+    setCounter(counter+1)
+    
+  }
+  const Decreament=()=>{
+    if(counter>0){
+      setCounter(counter-1)
+    }else{
+      alert("You Reached Zero Limit!")
+      setCounter(0)
+    } 
+  }
+
+  const setData=()=>{
+    alert("refresh")
+    localStorage.setItem("counter",counter)
+  }
 
   return (
     <>
-     <h1>hello brother</h1>
-    </>
-  )
+      <div className="container">
+      <h4>React Counter</h4>
+      <div className="box">
+        <div className="numeric"><h1>{counter}</h1></div>
+        <div className="btn">
+        <button className="incre" onLoad={setData} onClick={()=>Increament()}>+  Increament</button>
+        <button className="decre" onLoad={setData} onClick={()=>Decreament()}>-  Decreament</button>
+        </div>
+      </div>
+      </div>
+      </>
+  );
 }
 
-export default App
+export default Counter;
